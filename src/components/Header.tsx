14e599e08492d46ada9160afbe2fdf0f9c9ec9ba
ultradr3mer <ultradr3mer@gmail.com>
2025-07-19 13:48:37 +0200

    glass

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Container, Navbar, Nav } from 'react-bootstrap';
 
 export function Header() {
   return (
-    <Navbar bg="dark" variant="dark" className="justify-content-center">
+    <Navbar variant="dark" className="justify-content-center glass-blur">
       <Nav>
         <Nav.Link className="px-4">Menu</Nav.Link>
         <Nav.Link className="px-4">Menu</Nav.Link>
--- src/components/MenuTabs.tsx
+++ src/components/MenuTabs.tsx
@@ -1,8 +1,12 @@
+import { Container } from "react-bootstrap";
+
 export function MenuTabs() {
   return (
-    <div className="d-flex justify-content-center gap-3 my-4">
-      <button className="tab-btn">Menu</button>
-      <button className="tab-btn inactive">Menu</button>
-    </div>
+    <Container fluid className="text-white py-2 glass-blur">
+      <div className="d-flex justify-content-center gap-3">
+        <button className="tab-btn">Menu</button>
+        <button className="tab-btn inactive">Menu</button>
+      </div>
+    </Container>
   );
 }
